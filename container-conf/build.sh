66f1ca3747f4bf5885555d6fc118694a261932dc
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-05-23 14:22:36 -0600

    Fix #9 peg configurable-http-proxy@4.6.3 (#10)
    configurable-http-proxy moved to 5.x and broke configuration

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -16,9 +16,11 @@ build_as_root() {
 build_as_run_user() {
     umask 022
     mkdir -p "$HOME"/.local/{bin,lib}
-    npm install -g configurable-http-proxy
+    # POSIT: same version in radiasoft/sirepo/etc/run.sh
+    npm install --global configurable-http-proxy@4.6.3
     pip install wheel
     # POSIT: versions same in container-beamsim-jupyter/build.sh
+    # TODO(robnagler) version need to be the same in sirepo/etc/run.sh
     pip install jupyterhub==1.1.0 jupyterlab==2.1.0
     pip install ipywidgets
     pip install git+https://github.com/jupyterhub/oauthenticator.git@0.10.0
