21f3e637a8b3db457a02d52956cc7456bd69ca89
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-12-18 22:51:17 +0000

    updated some of the versions

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -19,24 +19,18 @@ build_as_run_user() {
     mkdir -p "$HOME"/.local/{bin,lib}
     _jupyterhub_nvm
     # POSIT: same version in radiasoft/sirepo/etc/run.sh
-    npm install --global configurable-http-proxy@4.6.3
-    pip install wheel
-    # POSIT: versions same in container-beamsim-jupyter/build.sh
-    # TODO(robnagler) version need to be the same in sirepo/etc/run.sh
-    pip install jupyterhub==1.1.0 jupyterlab==2.1.0
-    pip install ipywidgets
-    pip install git+https://github.com/jupyterhub/oauthenticator.git@0.10.0
-    pip install git+https://github.com/jupyterhub/dockerspawner.git@0.11.1
-    pip install git+https://github.com/radiasoft/pykern.git
-    pip install git+https://github.com/radiasoft/rsdockerspawner.git
-    pip install git+https://github.com/radiasoft/sirepo.git
+    npm install --global configurable-http-proxy
+    install_pip_install ipywidgets oauthenticator dockerspawner
+    # For testing, pull from local
+    install_pip_install pykern
+    install_pip_install git+https://github.com/radiasoft/rsdockerspawner.git
+    install_pip_install sirepo
 }
 
 _jupyterhub_nvm() {
     # Required when NVM_DIR is set
     mkdir -p "$NVM_DIR"
-    install_download https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh '' nvm 0.40.3 | PROFILE=/dev/null bash
+    PROFILE=/dev/null install_download https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh '' nvm 0.40.3 | PROFILE=/dev/null bash
     install_source_bashrc
-    # Matches nodejs on fedora 36
-    nvm install 16.18.1
+    nvm install 24.5.0
 }
